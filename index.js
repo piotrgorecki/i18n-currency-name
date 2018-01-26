fdd4c07f34f0a25e56978a470c75480d15851703
Piotr Górecki <piotr.gorecki@versum.pl>
2018-01-26 09:35:23 +0100

    Initialize

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var dictionaries = require("./src/dictionaries");
+
+/**
+ * Returns localized currency name
+ * @param {number} isoCode
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(isoCode, locale) {
+  if (!isoCode) return undefined;
+  if (!locale) return undefined;
+
+  var dictionary = dictionaries[locale];
+
+  if (dictionary) return dictionary[isoCode];
+  else return undefined;
+};
---
+++ src/dictionaries.js
@@ -0,0 +1,9 @@
+var en = require("./langs/en");
+var pl = require("./langs/pl");
+
+var dictionaries = {
+  en,
+  pl
+};
+
+module.exports = dictionaries;
